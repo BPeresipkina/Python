0a39c017a2c66402930d856182b6d6eedea0f4e5
Barbara Peres <barbarabelyaeva13@gmail.com>
2023-06-20 22:41:58 +0300

    added task

---
+++ lecture_1/main.py
@@ -0,0 +1,38 @@
+# (1)
+# a = 5
+# b = 5.89
+# c = 'apple'
+# print(f"{a}, {b}, {c}")
+# print("{}, {}, {}".format(a,b,c))
+
+# (2)
+# a = input("Введи число: ")
+# print(a)
+
+# (3)
+# c = 1
+# print(c)
+# print(type(c))
+# c = bool(c)
+# print(c)
+# print(type(c))
+
+# (4)
+# a = int(input("Введите первое число: "))
+# b = int(input("Введите второе число: "))
+# print(a, '+', b, '=', a + b)
+
+# (5)
+# a = 5.89956
+# b = 6.556551
+# print(round(a*b, 2))
+
+# (6)
+# a = 2
+# a += 3
+# print(a)
+
+(7)
+a = 'qwerty'
+for i in a:
+    print(i)
