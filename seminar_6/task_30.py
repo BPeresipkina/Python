30c0274e0b6fd3dec889bea9538b2fc070cd7646
Barbara Peres <barbarabelyaeva13@gmail.com>
2023-07-08 19:42:09 +0300

    added homework seminar 6

--- seminar_6/task_30.py
+++ seminar_6/task_30.py
@@ -8,3 +8,14 @@
 Вывод: 7 9 11 13 15
 '''
 
+a = int(input('Введите первое число: '))
+n = int(input('Введите кол-во элементов в последовательности: '))
+d = int(input('Введите шаг в последовательности: '))
+
+def massive_ap(a, n, d):
+    array = [a] * n
+    for i in range(1, n):
+        array[i] = array[i-1] + d
+    return array
+
+print(massive_ap(a, n, d))
--- seminar_6/task_32.py
+++ seminar_6/task_32.py
@@ -7,3 +7,15 @@
 Вывод: [1, 9, 13, 14, 19]
 '''
 
+min = int(input('Введите нижнюю границу диапазона: '))
+max = int(input('Введите верхнюю границу диапазона: '))
+array = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+
+def index_d(array, min, max):
+    massive = list()
+    for i in range(len(array)):
+        if array[i] <= max and array[i] >= min:
+            massive.append(i)
+    return massive
+
+print(index_d(array, min, max))
